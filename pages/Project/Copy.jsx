23c7057ef05459f10b0d0b5d8752b2d4d2b82de8
sun4205 <hanna4205@gmail.com>
2023-04-25 20:43:26 -0400

    practice project1

---
+++ pages/Project/Copy.jsx
@@ -0,0 +1,13 @@
+const Copy =() => {
+    const name ="hanna1234";
+
+
+    return (
+        <div style={{"backgroundColor": "blue"}}>
+            This is copy of project1 : {name}
+        </div>
+    )
+
+}
+
+export default Copy;
