335911b9d776b879c2cb8af036ffc28801a67576
sun4205 <hanna4205@gmail.com>
2023-04-29 19:19:14 -0400

    change tailwind

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Card from '../Components/Card'
 export default function Home() {
   return (
     <div>
-      <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+      <div class="p-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8">
         <Card name="Welcome"/>
         <Card name="Favorites"/>
         <Card name="Foods"/>
