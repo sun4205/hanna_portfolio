574b977333a1dede54414c5695bf5003ec602260
sun4205 <hanna4205@gmail.com>
2023-05-07 19:18:44 -0400

    change topbar

--- Components/Topbor.jsx
+++ Components/Topbor.jsx
@@ -1,12 +1,14 @@
+import '@/styles/globals.css'
 
 const Topbor = () => {
     return (
-        <div className="ToP">
-        <p class=" font-bold text-xl bg-blue-200 text-gray-700 text-base">
-                        This is practice. This is Topbar. Firt time i learned React.
-                     </p>
- 
-     </div>
+        <div>
+
+            <h1 className=" font-bold text-xl bg-blue-200 text-red-500 text-base">
+                This is practice. This is Topbar. 
+            </h1>
+
+        </div>
     )
 }
 export default Topbor;
