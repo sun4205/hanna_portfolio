a6a5a04d1c3354ac82ee8271e236e180c67fadf5
sun4205 <hanna4205@gmail.com>
2023-08-12 09:35:21 -0400

    change Portfolio1 contents

--- pages/Portfolio1.jsx
+++ pages/Portfolio1.jsx
@@ -4,7 +4,7 @@ const Portfolio1 = () => {
 
         <div>
             <h1 className=" font-bold text-xl bg-blue-200 text-red-500 text-base">
-        This is prahfhfhfhfhfhctice of Portfolio</h1>
+        It is Portfolio</h1>
         </div>
     )
 
