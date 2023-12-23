2d6ec6aa6616c07c2d50ecead196227723228560
sun4205 <hanna4205@gmail.com>
2023-12-23 15:46:50 -0500

    add content practice

--- Components/Topbor.jsx
+++ Components/Topbor.jsx
@@ -4,6 +4,7 @@ const Topbor = () => {
 
             <h1 className=" font-bold text-xl bg-blue-200 text-red-500 text-base">
                 This is practice. This is Topbar. 
+                This is practice for githup.
             </h1>
 
         </div>
